6eefcb22c1a17c5917d341db16f953205b7e9ef3
José Dhonatas Alves Sales <dhonatas.dev@gmail.com>
2022-05-14 11:31:01 -0300

    envio

--- codigo/olho_amigo1.ino/olho_amigo1.ino.ino
+++ codigo/olho_amigo1.ino/olho_amigo1.ino.ino
@@ -1,19 +1,18 @@
-//
+#include <Ultrasonic.h>
+
+HC_SR04 sensor1(6,5); // (trigger, echo)
+
+
 int duracao;
 int distancia;
 
 
 void setup()
 {
-  pinMode(13, OUTPUT); 
-  pinMode(12, OUTPUT); 
+
+  
   pinMode(11, OUTPUT); 
-  
   pinMode(10, OUTPUT);
-  
-  pinMode(2, OUTPUT);  
-  pinMode(3, INPUT);   
-  
   pinMode(9, OUTPUT); 
   Serial.begin(9600);
 
@@ -21,12 +20,9 @@ void setup()
 
 void loop()
 {
-digitalWrite(2,HIGH);
-delayMicroseconds(10);
-digitalWrite(2,LOW);
   
-duracao= pulseIn(3,HIGH);
-distancia= duracao * 0.034/2;
+
+distancia=   sensor1.distance();
   
 Serial.print("Distancia :");
 Serial.print(distancia);
@@ -34,7 +30,7 @@ Serial.print("cm");
 Serial.print("  ");
 
   
-  if(distancia>300){           
+  if(distancia>30){           
     digitalWrite(11,HIGH);
     digitalWrite(10,LOW);
     digitalWrite(9, LOW);
@@ -43,19 +39,19 @@ Serial.print("  ");
   else{
     digitalWrite(11,LOW);}
   
-  if(distancia>=150 && distancia<300){ 
+  if(distancia>=15 && distancia<30){ 
     digitalWrite(12,HIGH);
     digitalWrite(10,HIGH);
     digitalWrite(9, HIGH);
     //Serial.println("Intervalo medio");
-    delay(2000);
+    delay(200);
     digitalWrite(10,LOW);
     digitalWrite(9,LOW);
   }
   else{
        digitalWrite(12,LOW);}
      
-  if(distancia<150){                 
+  if(distancia<15){                 
     digitalWrite(13,HIGH);
     digitalWrite(10,HIGH);
     digitalWrite(9, HIGH);
